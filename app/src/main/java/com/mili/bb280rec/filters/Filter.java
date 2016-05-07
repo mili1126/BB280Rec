9df9ac7c3b5ae3c2776621dabacc5c7e5c332e1c
Ming Li <mili@mines.edu>
2016-05-07 16:56:33 -0600

    Update Filter class

--- app/src/main/java/com/mili/bb280rec/filters/Filter.java
+++ app/src/main/java/com/mili/bb280rec/filters/Filter.java
@@ -3,5 +3,9 @@ package com.mili.bb280rec.filters;
 /**
  * Created by mili on 5/7/16.
  */
-public class Filter {
+import org.opencv.core.Mat;
+
+public interface Filter {
+    public abstract int apply(final Mat src, final Mat dst);
 }
+
