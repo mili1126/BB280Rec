92f8d210b4b48e3289a9ad737af072aeb4e1ecc3
Ming Li <mili@mines.edu>
2016-05-07 15:43:29 -0600

    Basic Skeleton

---
+++ app/src/main/java/com/mili/bb280rec/CameraActivity.java
@@ -0,0 +1,13 @@
+package com.mili.bb280rec;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CameraActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
